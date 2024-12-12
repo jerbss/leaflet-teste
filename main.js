1d947f9838cf4d75e2c5ad19bb294972ed9362e3
Jerbesson Silva <jerbessonc@gmail.com>
2024-12-12 16:15:42 -0300

    criação projeto - primeiro mapa

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = init;
+
+function init() {
+    const mymap = L.map('mapid', {
+        center: [48, 14],
+        zoom: 4,
+        layers: [
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            })
+        ]
+    });
+}
