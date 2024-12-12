80650d7dad88c2de55038fba814d592a050c1973
Jerbesson Silva <jerbessonc@gmail.com>
2024-12-12 16:43:01 -0300

    mudança de coordenadas para Fortaleza - CE

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ window.onload = init;
 
 function init() {
     const mymap = L.map('mapid', {
-        center: [48, 14],
-        zoom: 4,
+        center: [-3.78222, -38.5434], // Coordenadas de Fortaleza - CE, Brasil
+        zoom: 12,
         layers: [
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
